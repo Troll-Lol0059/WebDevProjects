9dee1b34b75081bb5fe34dbbc1bbb4ba21165aa6
Troll-Lol0059 <akashshaw059@gmail.com>
2023-09-08 22:52:12 +0530

    Testimonial Slider Project using React-FinalCommit

--- testimonial-slider-project/src/components/Cards.js
+++ testimonial-slider-project/src/components/Cards.js
@@ -10,37 +10,39 @@ const Cards = (props) => {
     const setIndex = props.setIndex;
 
     function leftShiftHandler(){
-        if(index < 1){
-            console.log("I am called");
-            setIndex(1);
-        }
-        setIndex(index-1);
+        (index-1 < 0) ? setIndex(maxElement-1) : setIndex(index-1);
     }
 
     function rightShiftHandler(){
-        if(index > maxElement-1){
-            setIndex(0);
-        }
-        setIndex(index+1);
+        (index+1 > maxElement-1) ? setIndex(0):setIndex(index+1);
+    }
+
+    function generateRandomInteger(min,max){
+        return Math.floor(Math.random()*(max-min)-min);
+    }
+
+    function surpriseMeHandler(){
+        let randomInt = generateRandomInteger(0,maxElement);
+        setIndex(randomInt);
     }
 
     return (
         <div className="relative flex flex-col items-center justify-center p-16 pb-10">
             {/* for image */}
             <div className="rounded-full h-[19%] w-[19%] absolute top-[-20%] left-[6%]">
-                <img src={props.eachData.image} alt="cant load" className="object-contain rounded-full" />
+                <img src={props.loadData.image} alt="cant load" className="object-contain rounded-full" />
             </div>
 
             {/* for name */}
-            <div className="font-bold text-[1.5rem] tracking-wider">{props.eachData.name}</div>
+            <div className="font-bold text-[1.5rem] tracking-wider">{props.loadData.name}</div>
 
             {/* for designation */}
-            <div className="text-violet-500 uppercase text-[0.85rem] mb-8 mt-[-0.2rem]">{props.eachData.job}</div>
+            <div className="text-violet-500 uppercase text-[0.85rem] mb-8 mt-[-0.2rem]">{props.loadData.job}</div>
 
             <FaQuoteLeft size={iconSize} style={iconStyle} className="opacity-70" />
 
             {/* testimonial */}
-            <div className="my-4 opacity-60 text-[1rem]">{props.eachData.text}</div>
+            <div className="my-4 opacity-60 text-[1rem]">{props.loadData.text}</div>
 
             <FaQuoteRight size={iconSize} style={iconStyle} className="opacity-70" />
 
@@ -54,7 +56,7 @@ const Cards = (props) => {
             </div>
 
             <div className="mt-6 text-[1.05rem] text-white font-bold py-2 px-10 bg-violet-500 rounded-md opacity-70 cursor-pointer
-            transition-opacity  hover:opacity-100">Surprise Me</div>
+            transition-opacity  hover:opacity-100" onClick={surpriseMeHandler}>Surprise Me</div>
         </div>
     )
 }
--- testimonial-slider-project/src/components/HeroSection.js
+++ testimonial-slider-project/src/components/HeroSection.js
@@ -2,14 +2,12 @@ import Cards from "./Cards";
 
 const HeroSection = (props) => {
 
-    const loadData = [props.wholeData[props.index]];
+    const loadData = props.wholeData[props.index];
 
     return (
         <div className="w-[50%] h-[100vh] mt-12 rounded-md bg-[white]">
             {
-                loadData.map( (eachData)=> {
-                    return <Cards eachData={eachData} index={props.index} setIndex={props.setIndex} maxElement={props.maxElement} />
-                })
+                <Cards loadData={loadData} index={props.index} setIndex={props.setIndex} maxElement={props.maxElement} />
             }
         </div>
     )
