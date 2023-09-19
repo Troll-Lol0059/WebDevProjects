b56e3c6b9a69751c1e8a5212f873716d69eed7f7
Troll-Lol0059 <akashshaw059@gmail.com>
2023-09-19 23:26:05 +0530

    Router Project created using react- 4th commit

--- router-project/src/components/LoginForm.jsx
+++ router-project/src/components/LoginForm.jsx
@@ -1,16 +1,37 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { AiOutlineEye,AiOutlineEyeInvisible } from 'react-icons/ai';
+
 function LoginForm(){
+    const[loginFormData,setLoginFormData] = useState({email:"",password:""});
+    console.log(loginFormData);
+
+    function changeHandler(event){
+        const{name,value,type} = event.target;
+        setLoginFormData( (prev) => ({...prev, [name]:value}) );
+    }
+
     return (
         <div>
             <div className="flex flex-col">
                 <label htmlFor="userName">Email Address <sup>*</sup></label>
-                <input type="email" name="email" id="userName" placeholder="Enter Email Here" />
+                <input type="email" name="email" id="userName" placeholder="Enter Email Here" 
+                onChange={changeHandler}
+                value={loginFormData.email}
+                className="text-black" />
+            
             </div>
 
             <div className="flex flex-col">
                 <label htmlFor="password">Password <sup>*</sup></label>
-                <input type="password" name="password" id="password" placeholder="Enter Password" />
+                <input type="password" name="password" id="password" placeholder="Enter Password" 
+                onChange={changeHandler} 
+                value={loginFormData.password}
+                className="text-black"  />
+
+                <AiOutlineEye />
+                <AiOutlineEyeInvisible />
             </div>
 
             <div>
