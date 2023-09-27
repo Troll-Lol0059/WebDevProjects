85c4872161ce4f2c6c4285e5e037136aafa96c34
Troll-Lol0059 <akashshaw059@gmail.com>
2023-09-28 00:01:00 +0530

    Random GIF generator using React Custom Hook

--- random-gif-starter/src/components/Tag.jsx
+++ random-gif-starter/src/components/Tag.jsx
@@ -1,18 +1,44 @@
-import axios from "axios";
+import React, { useState } from 'react'
+import axios from 'axios';
+import { useEffect } from 'react';
+import Spinner from './Spinner';
+import useGif from '../hooks/useGif';
 
 
-export default function Tag() {
-  const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
-  const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
-  async function fetchData(){
-      let data = await fetch(url);
-      console.log(data);
-  }
+const Tag = () => {
+  const [tag, setTag] = useState('car');
 
-  fetchData();
+  const {gif, loading, fetchData} = useGif(tag);
 
-  return <div>
-    Tag
-    </div>;
+
+  return (
+    <div className='w-1/2  bg-blue-500 rounded-lg border border-black
+    flex flex-col items-center gap-y-5 mt-[15px]'>
+
+      <h1 className='mt-[15px] text-2xl underline uppercase font-bold'> Random {tag} Gif</h1>
+
+    {
+        loading ? (<Spinner/>) : (<img src= {gif} width="450" />)
+    }
+
+      <input 
+        className='w-10/12 text-lg py-2 rounded-lg mb-[3px] text-center'
+        onChange={(event) =>  setTag(event.target.value)}
+        value={tag}
+      />
+      
+
+      <button onClick={() => fetchData(tag)}
+      className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]">
+
+       Generate
+
+      </button>
+
+    </div>
+  )
 }
+
+export default Tag
