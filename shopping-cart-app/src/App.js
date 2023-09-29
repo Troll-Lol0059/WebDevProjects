cc2c3ec7ecc76bdf801a453f0981dbd3ad85b6ca
Troll-Lol0059 <akashshaw059@gmail.com>
2023-09-30 00:20:55 +0530

    Shopping Cart App created using React & Redux

---
+++ shopping-cart-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+
+  return (
+    <div className="App font-bold">
+      <div>Hello World !!</div>
+      <Routes>
+        <Route>
+        </Route>
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
