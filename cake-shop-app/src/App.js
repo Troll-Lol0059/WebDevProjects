768f3d7c6078f8567c4e24e96b31d6d8c5d96d99
Troll-Lol0059 <akashshaw059@gmail.com>
2023-11-26 12:22:00 +0530

    cake-shop using react initial commit - only Setup

---
+++ cake-shop-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App flex justify-center items-center">
+        Hello World !!
+    </div>
+  );
+}
+
+export default App;
